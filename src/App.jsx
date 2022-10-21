9abdc48ac9a6930f700db7a2242cd73d10c5bfed
Naval koranga <navalkoranga63@gmail.com>
2022-10-21 07:50:23 +0530

    added CreateBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
+import CreateBox from "./CreateBox";
 import Header from "./Header";
 import ToDoList from "./ToDoList";
 
 function App() {
   const [input, setInput] = useState(false);
   const [currentItem, setCurrentItem] = useState(""); //undefine - null
+  console.log("this is", currentItem);
   const [itemList, updateItemList] = useState([]);
+  const [buttonStatus, setButtonStatus] = useState(true);
+  const [done, setDone] = useState([]);
 
   function handleInputOpner() {
     setInput(!input);
   }
 
   function onChangeHandler(e) {
+    if (e.target.value.trim().length > 0) {
+      setButtonStatus(false);
+    } else {
+      setButtonStatus(true);
+    }
     setCurrentItem(e.target.value);
   }
 
   function addItemToList() {
+    setButtonStatus(true);
     updateItemList([...itemList, { item: currentItem, key: Date.now() }]);
     setCurrentItem("");
     console.log("string is ", itemList);
@@ -31,16 +41,16 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-green-500 text-center p-3 text-3xl">ToDo</h1>
-      <div className="py-10">
+      <h1 className="border shadow text-center p-3 text-3xl">To-Do</h1>
+      <div className="py-10 m-4">
         <div className="mx-auto max-w-7xl">
           <Header />
         </div>
 
-        <div className="mx-auto max-w-7xl">
-          <h3 className="text-xl">Things To Do</h3>
+        <div className="mx-auto mt-5 px-4 max-w-7xl">
+          <h3 className="text-xl text-gray-600 font-medium">Things to do</h3>
 
-          <ToDoList itemList={itemList} />
+          <ToDoList itemList={itemList} updateItemList={updateItemList} />
 
           {!input && (
             <button
@@ -53,40 +63,20 @@ function App() {
           )}
 
           {input && (
-            <div className="bg-white shadow sm:rounded-lg">
-              <div className="px-4 py-5 sm:p-6">
-                <h3 className="text-xl">Create a todo</h3>
-                <form className="mt-5"></form>
-                <div className="w-full sm:max-w-xs">
-                  <label htmlFor="to-do" className="sr-only">
-                    New Todo
-                  </label>
-                  <input
-                    value={currentItem}
-                    onChange={onChangeHandler}
-                    id="to-do"
-                    placeholder="Write a todo!!"
-                    className="block w-full py-2 px-3 rounded-md shadow-sm sm:text-sm"
-                  />
-                  <div className="flex justify-start gap-2 pt-5">
-                    <button
-                      onClick={addItemToList}
-                      className="inline-flex mt-2 items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-yellow-500 border border-transparent rounded-md shadow-sm hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-                    >
-                      Save
-                    </button>
-                    <button
-                      onClick={handleCancel}
-                      type="button"
-                      className="inline-flex mt-2 items-center px-3 py-2 text-sm font-medium leading-4 border border-black rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <CreateBox
+              currentItem={currentItem}
+              onChangeHandler={onChangeHandler}
+              buttonStatus={buttonStatus}
+              addItemToList={addItemToList}
+              handleCancel={handleCancel}
+            />
           )}
+
+          <div>
+            <h3 className="mt-5 text-xl text-gray-600 font-medium">
+              Things done
+            </h3>
+          </div>
         </div>
       </div>
     </>
